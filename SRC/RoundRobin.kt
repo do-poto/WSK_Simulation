fd99b1a487fb4b76de6a7ec4db6a6e284e2aff2a
cgdppl <dominikpotoczniak@gmail.com>
2025-05-27 21:48:56 +0200

    Other minor name changes

--- SRC/RoundRobin.kt
+++ SRC/RoundRobin.kt
@@ -1,9 +1,17 @@
-class RoundRobin{
+class RoundRobin(var processArray : ProcessObject){
+    //Set time quant
+    var timeQuant : Int = 3
+
+    //copy of the processArray
+
+
+}
+
+
+
+class main(){
 
 
 
 
-
-
-    
 }
