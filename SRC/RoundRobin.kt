aafb206b6a0dc22e3147535c1ca9a2063851a9e8
cgdppl <dominikpotoczniak@gmail.com>
2025-05-28 09:58:51 +0200

    Funciton started

--- SRC/RoundRobin.kt
+++ SRC/RoundRobin.kt
@@ -1,15 +1,41 @@
-class RoundRobin(var processArray : ProcessObject){
-    //Set time quant
-    var timeQuant : Int = 3
+class RoundRobin(){
+  //starts the process of executing the processes
+  class startExecution(){
+    //establish a time quant for each round
+    val timeQuant : Int = 3
 
-    //copy of the processArray
+    //counter for the amount of rounds needed until process completion
+    var turnCounter : Int = 0
+
+    //executes the processes 
+    fun executioner(var timeQuant : Int, var turnCounter : Int){
 
 
+
+
+    }
+    
+    //creates a process queue for the executioner
+    fun queueCreator(var processArray : Array<ProcessTestGenerator.ProcessObject?>, var turnCounter : Int){
+        var processQueue
+        
+        
+        for (item in processArray){
+            if(item?.arrivalTime <= turnCounter){
+
+            }
+        } 
+
+
+
+        return 
+    }
+  }
 }
 
 
 
-class main(){
+fun main(){
 
 
 
