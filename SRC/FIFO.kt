20a74b5dc24a875afee1d1d56891d603ba6e5271
cgdppl <dominikpotoczniak@gmail.com>
2025-05-29 21:44:35 +0200

    FIFO page algorithm corrected

--- SRC/FIFO.kt
+++ SRC/FIFO.kt
@@ -4,30 +4,36 @@ class FIFO{
     class Frame{
         //Creates a frame for the FIFO algorithm
         fun managePages(frameSize : Int, pagesArray : IntArray){
+            //page miss count
+            var pageMissCount = 0
+            //page hit count
+            var pageHitCount = 0
             //frame initialized
             var frameQueue : Queue<Int> = LinkedList()
             
             //creates actual queue size
-            var actualQueueSize : Int = frameQueue.size - 1
-            
+            var currentQueueSize : Int = 0
+            println(pagesArray.joinToString())
             //iterates through all of the proceess in the pagesArray
             for(page in pagesArray){
                 //check if he size is greater than expected
-                if(actualQueueSize >= frameSize){
+                if(frameQueue.contains(page)){
+                    //count hit
+                    pageHitCount++
+                } else if(currentQueueSize >= frameSize){
+                    //count miss
+                    pageMissCount++
                     //remove first item
                     frameQueue.poll()
                     //add new item
                     frameQueue.add(page)
                 } else {
+                    //count miss
+                    pageMissCount++
                     //otherwise add another page
                     frameQueue.add(page)
-                }
-                if(frameQueue.isEmpty() == false){
-                    var outline = ""
-                    for(item in frameQueue){
-                        outline = outline + item + ", "  
-                    }
-                    println(outline)
+                    //track queue size
+                    currentQueueSize++
                 }
             }
         }
@@ -40,6 +46,6 @@ fun main(){
     var arr = newGenerator.createPages(10)
 
     var FIFOAl = FIFO.Frame()
-    FIFOAl.managePages(3, arr)
+    FIFOAl.managePages(4, arr)
 
 }
