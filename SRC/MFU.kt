f6f80377a31240abcb8263293d531a06716ba3c9
cgdppl <dominikpotoczniak@gmail.com>
2025-05-29 21:30:35 +0200

    MFU corrected and given telemetry info

--- SRC/MFU.kt
+++ SRC/MFU.kt
@@ -4,41 +4,51 @@ class MFU{
     class Frame{
         //creates frame for the algorithm
         fun managePages(frameSize : Int, pagesArray : IntArray){
+            //page miss count
+            var pageMissCount = 0
+            //page hit count
+            var pageHitCount = 0
             //intialize array of int of certain size
             var frameArray = IntArray(frameSize)
 
-            //calculates the actual array size
-            var actualFrameArraySize : Int = frameSize - 1
-            var actualPageArraySize : Int = pagesArray.size - 1
-            //fill empty array
-            var i : Int= 0
-            while(i < actualFrameArraySize){
-                frameArray[i] = pagesArray[i]
-                i++
-            }
-            println(frameArray.joinToString())
-
-            //start MFU process
-            i++
-            while(i < actualPageArraySize){
-                //initialize highest usage 
-                var highestUsage : Int = -1
-                var highestUsageLocation : Int = -1
-                //find highest usage location
-                var j : Int = 0
-                while(j < actualFrameArraySize){
-                    if(frameArray[j] > highestUsage){
-                        highestUsage = frameArray[j]
-                        highestUsageLocation = j
+            //start process
+            for(pageOS in pagesArray){
+                var target = pageOS
+                //check if page exists in frame
+                if(target in frameArray){
+                    //page found
+                    pageHitCount++
+                } else if(0 in frameArray){
+                    //page not found and there is space in frame
+                    pageMissCount++
+                    var i : Int = 0
+                    //replace the empty space
+                    while(i < frameSize){
+                        if(frameArray[i] == 0){
+                            frameArray[i] = target
+                            break
+                        }
+                        i++
                     }
-                    j++
+                } else {
+                    //page not found and no more space in frame available
+                    pageMissCount++
+                    //initialize highest usage 
+                    var highestUsage : Int = -1
+                    var highestUsageLocation : Int = -1
+                    //find highest usage location
+                    var j : Int = 0
+                    //start MFU process
+                    while(j < frameSize){
+                        if(frameArray[j] > highestUsage){
+                            highestUsage = frameArray[j]
+                            highestUsageLocation = j
+                        }
+                        j++
+                    }
+                    frameArray[highestUsageLocation] = target
                 }
-                frameArray[highestUsageLocation] = pagesArray[i]
-                println(frameArray.joinToString())
-                i++
             }
-
-
         }
     }
 }
@@ -49,6 +59,6 @@ fun main(){
     var arr = newGenerator.createPages(10)
 
     var MFUAl = MFU.Frame()
-    MFUAl.managePages(3, arr)
+    MFUAl.managePages(4, arr)
 
 }
